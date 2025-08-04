1db466019b7155b1abaa5533a6eea5c67869621e
cESAR <elduro2002_@hotmail.com>
2025-08-04 15:53:33 -0400

    config backend project

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createUser(email: string, name?: string) {
+    return this.prisma.user.create({
+      data: {
+        email,
+        name,
+      },
+    });
+  }
+
+  async getUsers() {
+    return this.prisma.user.findMany();
+  }
+
+  async getUserById(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async updateUser(id: number, email?: string, name?: string) {
+    return this.prisma.user.update({
+      where: { id },
+      data: {
+        ...(email && { email }),
+        ...(name && { name }),
+      },
+    });
+  }
+
+  async deleteUser(id: number) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
